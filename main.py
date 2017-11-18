ea60c29e49f4fdc15c071b970e1b44d1676e59b3
sebastianfrey <sebastian.frey@outlook.com>
2017-11-18 23:08:17 +0100

    add cors support

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_restful import Api
+from flask_restful.utils import cors
 
 import server.resources as Resources
 
 APP = Flask(__name__)
 API = Api(APP)
+
+API.decorators = [cors.crossdomain(origin='*', headers=['accept', 'Content-Type'])] 
 
 
 API.add_resource(Resources.Services, '/')
